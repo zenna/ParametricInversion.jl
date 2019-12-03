49e672bb7c72bfc96afaa452234ea9ef01bcc406
Zenna Tavares <zennatavares@gmail.com>
2019-12-02 21:58:56 -0500

    working

--- src/Ailuj.jl
+++ src/Ailuj.jl
@@ -1,5 +1,25 @@
+"Jula in reverse: Parametric Inversion in pure Julia"
 module Ailuj
 
-greet() = print("Hello World!")
+using InteractiveUtils
+using IRTools
+using MacroTools
+using Spec
 
+using IRTools: Statement, varargs!, insertafter!, xcall, var, IR, Block, blocks, arguments
+using IRTools.Inner: Variable 
+using IRTools
+using IRTools: varargs!, inlineable!, pis!, slots!, IR, var, xcall
+using IRTools.Inner: argnames!, update!, argument!
+
+export invert
+# using ZenUtils
+
+include("duplify.jl")
+include("invert.jl")
+include("param.jl")
+include("primitives.jl")
+
+
+include("test.jl")
 end # module
